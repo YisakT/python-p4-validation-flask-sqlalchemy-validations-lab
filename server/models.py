b128feaf7be0f5917321a3fc765cba4abc82e409
yisak <yisakbruck23@gmail.com>
2023-09-16 19:44:40 -0400

    complete lab

--- server/models.py
+++ server/models.py
@@ -14,6 +14,18 @@ class Author(db.Model):
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
+    
+    @validates('name')
+    def validates_name(self,key,value):
+        if value == "":
+            raise ValueError("Name can not be empty")
+        return value
+    @validates('phone_number')
+    def validates_phone_number(self,key,phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must have 10 digits.")
+        else:
+            return phone_number
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -27,6 +39,31 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('content')
+    def validate_content(sefl,key,content):
+        if len(content) <= 250:
+            raise ValueError("Content can not be 250 characters long")
+        else:
+            return content
+    @validates('summary')
+    def validate_summary(sefl,key,summary):
+        if len(summary) >= 250:
+            raise ValueError("Summary must be less than 250 characters long")
+        else:
+            return summary
+        
+    @validates("category")
+    def validate_category(self,key,category):
+        if category  not in ["Fiction","Non-Fiction"]:
+            raise ValueError("Catagory must be Fiction or Non-Fiction")
+        return category
+    
+    @validates('title')
+    def validates_title(self,key,title):
+        if title not in ["Won't Believe","Secret","Top","Guess"]:
+            raise ValueError("Title is not click bait.")
+        return title
+    
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
